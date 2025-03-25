ff6a7504c7face2ec258c3ee88b929d5b5a0837e
DoggyDoggyDoggy <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-03-25 22:40:48 +1300

    Structure packages

--- app/src/main/java/denys/diomaxius/stopvape/MainActivity.kt
+++ app/src/main/java/denys/diomaxius/stopvape/MainActivity.kt
@@ -3,13 +3,7 @@ package denys.diomaxius.stopvape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import denys.diomaxius.stopvape.ui.screens.home.Home
 import denys.diomaxius.stopvape.ui.theme.StopVapeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             StopVapeTheme {
-
+                Home()
             }
         }
     }
---
+++ app/src/main/java/denys/diomaxius/stopvape/ui/screens/home/Home.kt
@@ -0,0 +1,9 @@
+package denys.diomaxius.stopvape.ui.screens.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Home() {
+    Text(text = "Home")
+}
